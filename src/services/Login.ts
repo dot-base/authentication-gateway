9d6af70c3e69cbff090f9fa0125d4126793aec6e
Christian Friedow <christian@friedow.com>
2021-10-25 15:39:39 +0200

    Move encryption to crypto service

--- src/services/Login.ts
+++ src/services/Login.ts
@@ -1,16 +1,9 @@
-import CryptoJS from "crypto-js";
-
 import KeycloakApi from "@/api/keycloak";
+import CryptoService from "@/services/Crypto";
 
 export default class LoginService {
-  private static get aesPassphrase(): string {
-    if (!process.env.COOKIE_ENCRYPTION_PASSPHRASE_AES)
-      throw new Error("AES passphrase not defined.");
-    return process.env.COOKIE_ENCRYPTION_PASSPHRASE_AES;
-  }
-
   public static async createSessionCookie(username: string, password: string): Promise<string> {
     const tokens = await KeycloakApi.login(username, password);
-    return CryptoJS.AES.encrypt(JSON.stringify(tokens), this.aesPassphrase).toString();
+    return CryptoService.encrypt(tokens);
   }
 }
