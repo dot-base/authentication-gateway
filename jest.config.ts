9a510238e84e244cf77e6312b12ce054d9d629ab
Christian Friedow <christian@friedow.com>
2021-11-24 14:17:15 +0100

    test: Add tests for the login, renewal and validation endpoints
    ### 🚀 Feature Description
    This PR introduces tests for all current endpoints. These are: the login, renewal and validation endpoints.
    
    ### 🧰 Technical Solution
    - [x] Add tests for the login, validation and renewal endpoint using the jest framework
    - [x] Add tests for sucessful as well as unsuccessful usage

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    moduleNameMapper: {
+      "^@/(.*)$": "<rootDir>/src/$1",  // map @ to absolute path,
+      "^@tests/(.*)$": "<rootDir>/tests/$1"
+    },
+};
