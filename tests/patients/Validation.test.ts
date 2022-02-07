7aac344342f86dbdd0c4484e1ae430211928909d
Andrea <andrea@kreichgauer.com>
2022-02-07 11:05:25 +0100

    rm validation test with notExpired cookie for patient realm

--- tests/patients/Validation.test.ts
+++ tests/patients/Validation.test.ts
@@ -2,23 +2,11 @@ import { Describe, Test } from "jest-decorator";
 import request from "supertest";
 
 import express from "@/express";
-import MockTokenIntrospection from "@tests/__mocks__/TokenIntrospection";
-import MockCookie from "@tests/__mocks__/Cookie";
 
 jest.mock("@/api/Keycloak");
 
 @Describe("Validation endpoint for a patient user")
 export default class ValidationTestGroup {
-  @Test("should respond with HTTP status 200 if a valid session cookie is submitted")
-  private async testValidSessionCookie() {
-    await request(express)
-      .get("/api/auth/validate")
-      .set("Cookie", MockCookie.notExpiredPatientRealm)
-      .expect(200)
-      .expect("X-Forwarded-User", MockTokenIntrospection.patientRealm.email ?? "")
-      .expect("X-Auth-Realm", process.env.KEYCLOAK_PATIENT_REALM_NAME ?? "patients");
-  }
-
   @Test(
     "should respond with HTTP status 307 if a session cookie with an expired access_token is submitted"
   )
