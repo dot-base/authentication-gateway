860796b8215ae15aaf28038c3f99f884df9cbc39
andreakreichgauer <38720222+andreakreichgauer@users.noreply.github.com>
2022-02-07 12:01:22 +0100

    feat: add implicit cookie refresh on cookie validation
    This introduces/fixes/improves feature/bug/enhancement description. This closes #1337.
    
    BREAKING CHANGE: Endpoint for cookie renewal will be removed and cookie will implicitly be refreshed on validation

--- src/services/Cookie.ts
+++ src/services/Cookie.ts
@@ -1,39 +1,55 @@
 import KeycloakApi from "@/api/Keycloak";
 import RealmFactory from "@/models/RealmFactory";
 import CryptoService from "@/services/Crypto";
+import Cookie from "@/types/Cookie";
 import RealmConfig from "@/types/RealmConfig";
 import TokenIntrospection from "@/types/TokenIntrospection";
+import Tokens from "@/types/Tokens";
 import UserInfo from "@/types/UserInfo";
 import JwtUtil from "@/utils/Jwt";
 
 export default class CookieService {
+  private static createCookieFromToken(realm: RealmConfig, tokens: Tokens) {
+    return {
+      value: CryptoService.encrypt(realm, tokens),
+      options: {
+        expires: new Date(Date.now() + tokens.refresh_expires_in * 1000),
+        httpOnly: true,
+      },
+    };
+  }
   public static async createSessionCookie(
     realm: RealmConfig,
     username: string,
     password: string
-  ): Promise<string> {
+  ): Promise<Cookie> {
     const tokens = await KeycloakApi.login(realm, username, password);
-    return CryptoService.encrypt(realm, tokens);
+    return this.createCookieFromToken(realm, tokens);
   }
 
-  public static async renewSessionCookie(
-    realm: RealmConfig,
-    sessionCookie: string
-  ): Promise<string> {
+  public static async validateCookieExpiration(sessionCookie: string): Promise<boolean> {
     const tokens = CryptoService.decrypt(sessionCookie);
-    const newTokens = await KeycloakApi.refresh(realm, tokens.refresh_token);
-    return CryptoService.encrypt(realm, newTokens);
+    return JwtUtil.isExpired(tokens.access_token);
   }
 
   public static async validateSessionCookie(sessionCookie: string): Promise<TokenIntrospection> {
     const tokens = CryptoService.decrypt(sessionCookie);
     const realmName = JwtUtil.getRealmName(tokens);
     const realm = RealmFactory.realm(realmName);
-    const inspectedToken = await KeycloakApi.validate(realm, tokens);
 
+    const inspectedToken = await KeycloakApi.validate(realm, tokens);
     if (!inspectedToken.active) throw new Error("Access token is invalid.");
 
     return inspectedToken;
+  }
+
+  public static async renewSessionCookie(sessionCookie: string): Promise<Cookie> {
+    const tokens = CryptoService.decrypt(sessionCookie);
+    const realmName = JwtUtil.getRealmName(tokens);
+    const realm = RealmFactory.realm(realmName);
+
+    const newTokens = await KeycloakApi.refresh(realm, tokens.refresh_token);
+    return this.createCookieFromToken(realm, newTokens);
   }
 
   public static async invalidateSessionCookie(sessionCookie: string): Promise<void> {
--- src/utils/Jwt.ts
+++ src/utils/Jwt.ts
@@ -1,39 +1,32 @@
 import TokenIntrospection from "@/types/TokenIntrospection";
 import Tokens from "@/types/Tokens";
-import UserInfo from "@/types/UserInfo";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
 export default class JwtUtil {
-  public static decode(accessToken: string): JwtPayload {
-    const decoded = jwt.decode(accessToken);
-    if (!decoded) throw new Error("Access token is invalid.");
+  public static decode(token: string): JwtPayload {
+    const decoded = jwt.decode(token);
+    if (!decoded) throw new Error("Token is invalid.");
     return decoded as JwtPayload;
   }
 
   public static getTokenIssuerRealm(token: TokenIntrospection): string {
     const realmName = token.iss?.split("/").pop();
-    if (!realmName) throw new Error("Access token is invalid.");
+    if (!realmName) throw new Error("Token is invalid.");
     return realmName;
   }
 
   public static getRealmName(tokens: Tokens): string {
     const jwtPayload = JwtUtil.decode(tokens.access_token);
     const realmName = jwtPayload.iss?.split("/").pop();
-    if (!realmName) throw new Error("Access token is invalid.");
+    if (!realmName) throw new Error("Token is invalid.");
     return realmName;
   }
 
-  public static getUserInfo(tokens: Tokens): UserInfo {
-    const jwtPayload = JwtUtil.decode(tokens.access_token);
-    const userInfo: UserInfo = {
-      preferred_username: jwtPayload.preferred_username,
-      name: jwtPayload.name,
-      given_name: jwtPayload.given_name,
-      family_name: jwtPayload.family_name,
-      email: jwtPayload.email,
-      email_verified: jwtPayload.email_verified,
-      realm_access_roles: jwtPayload.realm_access.roles,
-    };
-    return userInfo;
+  public static isExpired(token: string): boolean {
+    const expireTime = JwtUtil.decode(token).exp;
+
+    if (!expireTime) return true;
+
+    return new Date(expireTime * 1000).toISOString() < new Date().toISOString();
   }
 }
