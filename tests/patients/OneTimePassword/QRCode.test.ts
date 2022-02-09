2c488cf854e6e2c26fdc6e3c5bf1f9467591a7f2
Andrea <andrea@kreichgauer.com>
2022-02-09 15:36:35 +0100

    refactor tests

--- tests/patients/OneTimePassword/QRCode.test.ts
+++ tests/patients/OneTimePassword/QRCode.test.ts
@@ -19,16 +19,9 @@ export default class OneTimePasswordTestGroup {
     const res = await request(express)
       .get("/api/auth/totp/qrcode/testpatient")
       .set("Cookie", cookie)
+      .set("x-auth-realm", process.env.KEYCLOAK_DOTBASE_REALM_NAME ?? "dotbase")
       .expect(200);
     expect(res.body).toHaveProperty("qrCode");
-  }
-
-  @Test("should respond with HTTP status 401 if a session cookie is submitted which does not include a valid token")
-  private async testQRCodeInvalidSessionCookie() {
-    await request(express)
-      .get("/api/auth/totp/qrcode/testpatient")
-      .set("Cookie", "session=some-invalid-cookie-value")
-      .expect(401);
   }
 
   @Test("should respond with HTTP status 403 if a not authorized user (e.g. patient) requests a qrCode")
@@ -43,11 +36,12 @@ export default class OneTimePasswordTestGroup {
     await request(express)
       .get("/api/auth/totp/qrcode/testpatient")
       .set("Cookie", cookie)
+      .set("x-auth-realm", process.env.KEYCLOAK_PATIENT_REALM_NAME ?? "patients")
       .expect(403);
   }
 
   @Test("should respond with HTTP status 401 if the session cookie is missing")
   private async testQRCodeMissingSessionCookie() {
-    await request(express).get("/api/auth/totp/qrcode/test").expect(401);
+    await request(express).get("/api/auth/totp/qrcode/test").set("x-auth-realm", process.env.KEYCLOAK_DOTBASE_REALM_NAME ?? "dotbase").expect(401);
   }
 }
